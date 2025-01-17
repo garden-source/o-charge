a5497f6fdaedd8a4a1c90897de416beac80269a5
garden-source <nagashima@garden-jp.biz>
2025-01-17 16:28:38 +0900

    フッターメニュー完成

--- components/FooterMenu.tsx
+++ components/FooterMenu.tsx
@@ -1,36 +1,20 @@
 // components/FooterMenu.tsx
-export default function FooterMenu() {
+const FooterMenu = () => {
   return (
-    <nav>
-      <ul className="flex gap-4">
-        <li>
-          <a
-            href="mailto:info@example.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            メール
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://www.kakaocorp.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            カカオトーク
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://line.me/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            LINE
-          </a>
-        </li>
-      </ul>
+    <nav className="bg-[#4b4a4d] p-3 max-w-screen-md mx-auto fixed bottom-0 left-0 right-0 z-50">
+      <div className="flex justify-between space-x-4 px-4">
+        <div className="flex flex-col items-center flex-1 border-r border-gray-300 min-w-[150px]">
+          <img src="/images/Other/mail-icon.png" alt="メール" className="h-10 w-auto" />
+        </div>
+        <div className="flex flex-col items-center flex-1 border-r border-gray-300 min-w-[150px]">
+          <img src="/images/Other/kakao-icon.png" alt="カカオトーク" className="h-10 w-auto" />
+        </div>
+        <div className="flex flex-col items-center flex-1 min-w-[150px]">
+          <img src="/images/Other/line-icon.png" alt="LINE" className="h-10 w-auto" />
+        </div>
+      </div>
     </nav>
   );
-}
+};
+
+export default FooterMenu;
